ea86301276356bd37ec78954f833534cb5d7d7c2
Thomas Pan <tpan@e1z2r8p25.42.us.org>
2016-12-15 00:36:55 -0800

    initial commit, added author file, header file with necessary includes

---
+++ get_next_line.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tpan <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/11 21:03:27 by tpan              #+#    #+#             */
+/*   Updated: 2016/12/15 00:35:48 by tpan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft/libft.h"
+#include "get_next_line.h"
+
+int get_next_line(const int fd, char **line)
+{
+	char *ptr_buff;
+
+
+	read(fd,buff,
+
+}
+
+int main (int argc, char *argv[])
+{
+	if (argc != 1)
+	{
+		write(1,"nope", 4);
+	}
+	argv = 0;
+	return (0);
+}
