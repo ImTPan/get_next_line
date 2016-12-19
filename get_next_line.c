938b9a675cd3c066f7279ed84f5f2edf2e0766b3
Thomas Pan <tpan@e1z2r5p10.42.us.org>
2016-12-19 12:56:32 -0800

    finished get_next_line main function, debugging; current error is not finding the EOF

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tpan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/11 21:03:27 by tpan              #+#    #+#             */
-/*   Updated: 2016/12/18 16:48:59 by tpan             ###   ########.fr       */
+/*   Updated: 2016/12/19 12:51:09 by tpan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,17 +30,45 @@ static t_list		*read_fd(int fd)
 	return (tmp);
 }
 
-static void			join(t_list *node, size_t size)
+static void			join(t_list *node, char const *buff, size_t size)
 {
-	
+	char	*tmp;
+
+	if (node->content == NULL)
+	{
+		node->content = ft_strndup(buff, size);
+		return ;
+	}
+	tmp = node->content;
+	node->content = ft_strjoin(tmp, buff);
+	free((void *)buff);
 }
 
-int get_next_line(const int fd, char **line)
+int	get_next_line(const int fd, char **line)
 {
-	static char *parse = NULL;
-	char		*endl_index;
+	char		*ptr;
+	char		buff[BUFF_SIZE];
 	size_t		ret;
+	size_t		i;
+	t_list		*nd;
 
-	MEMCHECK(!parse && (parse = (char *)(ft_memalloc(sizeof char))) == NULL)
-
+	i = 0;
+	ret = 0;
+	MEMCHECK((fd < 0) || (!line) || read(fd, buff, BUFF_SIZE) < 0);
+	nd = read_fd(fd);
+	while (!ft_strchr(nd->content, '\n') && (ret = read(fd, buff,BUFF_SIZE)))
+		join(nd, ft_strndup(buff, ret), ret);
+	if (ret < BUFF_SIZE && ft_strlen(nd->content) == 0)
+	{
+		ft_bzero(&line, ft_strlen(*line));
+		return (0);
+	}
+	ptr = nd->content;
+	i = ft_wordlength(ptr, i, '\n');
+	*line = (ptr[i] == '\n') ? (ft_strndup(ptr, i)) : (ft_strdup(nd->content));
+	if ((ret == 0 && ptr[i] == 0))
+		ft_bzero(nd->content,(ft_strlen(nd->content)));
+	nd->content = (ptr[i] == '\n') ? (ft_strdup(nd->content + (i + 1))) :
+		(nd->content + 0);
+	return (1);
 }
