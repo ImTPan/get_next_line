fd50456b5707021fa9f9add2e12de4cab674f648
Thomas Pan <tpan@e1z2r5p10.42.us.org>
2016-12-20 00:00:17 -0800

    still debugging, head being set to null somewhere it should not

--- get_next_line.c
+++ get_next_line.c
@@ -6,12 +6,11 @@
 /*   By: tpan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/11 21:03:27 by tpan              #+#    #+#             */
-/*   Updated: 2016/12/19 15:29:42 by tpan             ###   ########.fr       */
+/*   Updated: 2016/12/19 17:40:47 by tpan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include "libft/libft.h"
 
 static t_list		*read_fd(int fd)
 {
@@ -19,7 +18,7 @@ static t_list		*read_fd(int fd)
 	t_list			*tmp;
 
 	tmp = read_head;
-	while (tmp != '\0')
+	while (tmp)
 	{
 		if ((int)tmp->content_size == fd)
 			return (tmp);
@@ -47,28 +46,28 @@ static void			join(t_list *node, char const *buff, size_t size)
 
 int					get_next_line(const int fd, char **line)
 {
+	int				i;
+	int				ret;
 	char			*ptr;
-	char			buff[BUFF_SIZE];
-	size_t			ret;
-	size_t			i;
+	char			buf[BUFF_SIZE];
 	t_list			*nd;
 
 	i = 0;
 	ret = 0;
-	MEMCHECK((fd < 0) || (!line) || read(fd, buff, BUFF_SIZE) < 0);
+	MEMCHECK(fd < 0 || !(line == NULL) || read(fd, buf, 0) < 0);
 	nd = read_fd(fd);
-	while (!ft_strchr(nd->content, '\n') && (ret = read(fd, buff, BUFF_SIZE)))
-		join(nd, ft_strndup(buff, ret), ret);
+	while (!ft_strchr(nd->content, '\n') && (ret = read(fd, buf, BUFF_SIZE)))
+		join(nd, ft_strndup(buf, ret), ret);
 	if (ret < BUFF_SIZE && ft_strlen(nd->content) == 0)
 	{
-		ft_bzero(&line, ft_strlen(*line));
+		ft_strclr(*line);
 		return (0);
 	}
 	ptr = nd->content;
 	i = ft_wordlength(ptr, i, '\n');
 	*line = (ptr[i] == '\n') ? (ft_strndup(ptr, i)) : (ft_strdup(nd->content));
 	if ((ret == 0 && ptr[i] == 0))
-		ft_bzero(nd->content, (ft_strlen(nd->content)));
+		ft_strclr((char*)(nd->content));
 	nd->content = (ptr[i] == '\n') ? (ft_strdup(nd->content + (i + 1))) :
 		(nd->content + 0);
 	return (1);
