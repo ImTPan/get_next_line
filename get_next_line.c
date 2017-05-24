c9bce32b7a6b4821e3a3909bbe13defa44560405
Thomas Pan <tpan@Thomas-MacBook-Pro.local>
2017-05-24 14:29:02 -0700

    fixing gnl leak

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tpan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/11 21:03:27 by tpan              #+#    #+#             */
-/*   Updated: 2016/12/20 01:50:53 by tpan             ###   ########.fr       */
+/*   Updated: 2017/05/24 14:15:29 by tpan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,6 +44,15 @@ static void			join(t_list *node, char const *buff, size_t size)
 	free((void *)buff);
 }
 
+int					ft_wrdlen(char *str, int i, char delimiter)
+{
+	while (str[i] != '\0' && str[i] != delimiter)
+	{
+		i++;
+	}
+	return (i);
+}
+
 int					get_next_line(const int fd, char **line)
 {
 	size_t				i;
@@ -54,7 +63,7 @@ int					get_next_line(const int fd, char **line)
 
 	i = 0;
 	ret = 0;
-	MEMCHECK((fd < 0 || line == NULL || read(fd, buf, 0) < 0));
+	ERRCHECK((fd < 0 || line == NULL || read(fd, buf, 0) < 0));
 	nd = read_fd(fd);
 	while (!ft_strchr(nd->content, '\n') && (ret = read(fd, buf, BUFF_SIZE)))
 		join(nd, ft_strndup(buf, ret), ret);
@@ -64,7 +73,7 @@ int					get_next_line(const int fd, char **line)
 		return (0);
 	}
 	ptr = nd->content;
-	i = ft_wordlength(ptr, i, '\n');
+	i = ft_wrdlen(ptr, i, '\n');
 	*line = (ptr[i] == '\n') ? (ft_strndup(ptr, i)) : (ft_strdup(nd->content));
 	if ((ret == 0 && ptr[i] == 0))
 		ft_strclr((char*)(nd->content));
--- get_next_line.h
+++ get_next_line.h
@@ -6,20 +6,18 @@
 /*   By: tpan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/14 18:22:44 by tpan              #+#    #+#             */
-/*   Updated: 2016/12/18 14:38:07 by tpan             ###   ########.fr       */
+/*   Updated: 2017/05/24 14:10:01 by tpan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# define BUFF_SIZE 1024
+
 # include <stdlib.h>
 # include <unistd.h>
 # include "libft/libft.h"
-
-# define BUFF_SIZE 32
-
-# define MEMCHECK(x) if (x) return (-1);
 
 int	get_next_line(const int fd, char **line);
 #endif
